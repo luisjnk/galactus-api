bb6bca47386c7109c12d9f17f8e255233ce5b7c2
luis <luisjnk@gmail.com>
2020-04-30 21:09:59 +0100

    add eslint config

--- presentation/controllers/signup.ts
+++ presentation/controllers/signup.ts
@@ -1,7 +1,15 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    } if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
     }
   }
 }
