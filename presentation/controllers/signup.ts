fb080a5ef3aa3fb8edcbc9cdf1112be84648d9dd
luis <luisjnk@gmail.com>
2020-04-30 22:04:27 +0100

    Refactor, moved duplicated code

--- presentation/controllers/signup.ts
+++ presentation/controllers/signup.ts
@@ -1,19 +1,14 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     console.log(httpRequest.body.name)
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+      return badequest(new MissingParamError('name'))
     } if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
+      return badequest(new MissingParamError('email'))
     }
   }
 }
